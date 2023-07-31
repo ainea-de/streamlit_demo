ede08f1f29e3ee0dfbdbb71aafd4a6c529970378
ainea-de <140893741+ainea-de@users.noreply.github.com>
2023-07-31 09:06:59 +0200

    Create streamlit_sst.py

---
+++ streamlit_sst.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+# create an empty dataframe to store the input text
+df = pd.DataFrame(columns=['Input Text'])
+
+# define the label
+st.write('Enter text:')
+
+# define the input field
+user_input = st.text_input(label='')
+
+# define the button
+button_clicked = st.button('Submit')
+
+# check if button is clicked
+if button_clicked:
+    # add the user input to the dataframe
+    df.loc[len(df)] = [user_input]
+    
+    # display the table
+    st.table(df)
